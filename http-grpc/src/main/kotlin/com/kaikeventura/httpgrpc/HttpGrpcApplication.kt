2e0c59d1c820d343cbc7729d5effc8df8f47422d
kaikeventura <kaike.gnr@hotmail.com>
2024-03-05 23:44:32 -0300

    Criado um builder para persistir a base

--- http-grpc/src/main/kotlin/com/kaikeventura/httpgrpc/HttpGrpcApplication.kt
+++ http-grpc/src/main/kotlin/com/kaikeventura/httpgrpc/HttpGrpcApplication.kt
@@ -1,11 +1,46 @@
 package com.kaikeventura.httpgrpc
 
+import com.kaikeventura.httpgrpc.entity.TaxType
+import com.kaikeventura.httpgrpc.service.CompanyService
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
 class HttpGrpcApplication
 
 fun main(args: Array<String>) {
 	runApplication<HttpGrpcApplication>(*args)
+}
+
+@Component
+class Init(
+	private val companyService: CompanyService
+) {
+
+	@PostConstruct
+	fun start() {
+		val company = companyService.createCompany(
+			companyName = "Coquinha"
+		)
+
+		companyService.addTax(
+			companyId = company.id!!,
+			taxType = TaxType.IOF,
+			percentage = 1.5
+		)
+
+		companyService.addTax(
+			companyId = company.id,
+			taxType = TaxType.IPI,
+			percentage = 19.2
+		)
+
+		companyService.addTax(
+			companyId = company.id,
+			taxType = TaxType.PIS,
+			percentage = 13.4
+		)
+	}
 }
