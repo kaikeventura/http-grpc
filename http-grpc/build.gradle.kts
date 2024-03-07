f163a5b881105922390d48b9417558680c8d53bb
kaikeventura <kaike.gnr@hotmail.com>
2024-03-07 00:35:05 -0300

    Configuração do grpc

--- http-grpc/build.gradle.kts
+++ http-grpc/build.gradle.kts
@@ -1,8 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.google.protobuf.gradle.*
 
 plugins {
 	id("org.springframework.boot") version "3.2.3"
 	id("io.spring.dependency-management") version "1.1.4"
+	id ("com.google.protobuf") version "0.8.18"
 	kotlin("jvm") version "1.9.22"
 	kotlin("plugin.spring") version "1.9.22"
 	kotlin("plugin.jpa") version "1.9.22"
@@ -24,6 +26,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("io.grpc:grpc-netty:1.44.1")
+	implementation("io.grpc:grpc-protobuf:1.44.1")
+	implementation("io.grpc:grpc-kotlin-stub:1.2.1")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
@@ -37,4 +42,32 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+protobuf {
+	generatedFilesBaseDir = "$buildDir/generated/sources/proto"
+	protoc {
+		artifact = "com.google.protobuf:protoc:3.12.2"
+	}
+
+	plugins {
+		id("grpc") {
+			artifact = "io.grpc:protoc-gen-grpc-java:1.30.0"
+		}
+	}
+	generateProtoTasks.all().forEach {
+		it.plugins {
+			id("grpc"){
+				outputSubDir = "grpc"
+			}
+		}
+	}
+}
+
+sourceSets{
+	main{
+		proto{
+			srcDir("$buildDir/generated/sources/proto/main/grpc")
+		}
+	}
 }
